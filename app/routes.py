71b0594655cdcfa2677da96d4e8fa2dcea73b444
niharikagali <niharikagali23@gmail.com>
2018-04-02 17:11:39 +0530

    changes made

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
+from flask import render_template
 from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username':'Niharika'}
+    return render_template('index.html',title='Home',user=user)
