143dfd0b4475ec549d04f3d33f35a3db207f782c
niharikagali <niharikagali23@gmail.com>
2018-04-03 23:35:08 +0530

    Login Form updated

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,8 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
 	username = StringField('Username',validators = [DataRequired()])
 	password = PasswordField('Password', validators = [DataRequired()])
 	remember_me = BooleanField('Remember Me')
