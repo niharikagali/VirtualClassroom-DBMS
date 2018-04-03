f71c144cbe4938355ff86d49b22a8a0adf4153de
niharikagali <niharikagali23@gmail.com>
2018-04-03 21:18:00 +0530

    login added

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,6 @@
 from flask import render_template
 from app import app
-<<<<<<< HEAD
 from app.forms import LoginForm
-=======
->>>>>>> 6fecdfa6a9a2fde248a3a3696bfeadfa558dd100
 @app.route('/')
 @app.route('/index')
 def index():
@@ -18,13 +15,10 @@ def index():
     		'body' : '80!'
     	}
     ]
-<<<<<<< HEAD
     return render_template('index.html',title='Home',user=user,notifs = notifs)
 
 @app.route('/login')
 def login():
 	form = LoginForm()
 	return render_template('login.html',title='Sign In',form = form)
-=======
-    return render_template('index.html',title='Home',user=user,notifs = notifs)
->>>>>>> 6fecdfa6a9a2fde248a3a3696bfeadfa558dd100
+    
